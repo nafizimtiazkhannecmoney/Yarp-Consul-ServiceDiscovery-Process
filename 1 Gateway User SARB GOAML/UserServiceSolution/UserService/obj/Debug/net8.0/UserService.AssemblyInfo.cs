9f2a1acaa3ed313f0480e166e3a31246fb3d601e
nafizimtiazkhannecmoney <nafiz@primotechltd.com>
2025-07-13 13:26:36 +0600

    Update

--- 1 Gateway User SARB GOAML/UserServiceSolution/UserService/obj/Debug/net8.0/UserService.AssemblyInfo.cs
+++ 1 Gateway User SARB GOAML/UserServiceSolution/UserService/obj/Debug/net8.0/UserService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ece92d30e45011054a1d6973fa7de15c0cd9b78")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
